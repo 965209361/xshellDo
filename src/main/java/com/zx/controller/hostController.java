2121627b7432fd383cc03673c0cbcc6834a186f9
zengqt <zengqintao@zxisl.com>
2018-05-16 13:55:58 +0800

    服务数据修改

--- src/main/java/com/zx/controller/hostController.java
+++ src/main/java/com/zx/controller/hostController.java
@@ -11,26 +11,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * created by zengqintao on 2018-05-15 10:50 .
  **/
 @Controller
-@RequestMapping("/xshell")
 public class hostController {
 
     @RequestMapping("/host")
-    public String host(Host host, Model model){
+    public String host(Host host, Model model) {
         String name = host.getAccount();
-        if(!StringUtils.isEmpty(name)&&!StringUtils.isEmpty(host.getHost())&&!StringUtils.isEmpty(host.getPassword())){
-            model.addAttribute("InfoSucc","端口数据存入成功!");
-        }else
-            model.addAttribute("InfoError","端口数据为空,请重试");
+        if (StringUtils.isEmpty(name)) {
+            model.addAttribute("InfoError", "账号值为空,请重试");
+        } else if (StringUtils.isEmpty(host.getHost())) {
+            model.addAttribute("InfoError", "端口号为空,请重试");
+        } else if (StringUtils.isEmpty(host.getPassword())) {
+            model.addAttribute("InfoError", "密码值为空,请重试");
+        } else if (!StringUtils.isEmpty(name) && !StringUtils.isEmpty(host.getHost()) && !StringUtils.isEmpty(host.getPassword())) {
+            model.addAttribute("InfoSucc", "端口数据存入成功!");
+        }
         return "views/add";
     }
 
     @RequestMapping("/service")
-    public String service(ServiceDomain serviceDomain,Model model){
+    public String service(ServiceDomain serviceDomain, Model model) {
         String name = serviceDomain.getProcess_name();
-        if(!StringUtils.isEmpty(name)){
-            model.addAttribute("InfoSucc","服务数据存入成功!");
-        }else
-            model.addAttribute("InfoError","服务数据为空,请重试");
+        if (StringUtils.isEmpty(serviceDomain.getService_name())) {
+            model.addAttribute("InfoError", "服务器名为空,请重试");
+        } else if (StringUtils.isEmpty(name)) {
+            model.addAttribute("InfoError", "process..为空,请重试");
+        } else if (StringUtils.isEmpty(serviceDomain.getPath())) {
+            model.addAttribute("InfoError", "路径为空,请重试");
+        } else if (StringUtils.isEmpty(serviceDomain.getCmad_start())) {
+            model.addAttribute("InfoError", "Cmad值为空,请重试");
+        } else if (!StringUtils.isEmpty(name) && !StringUtils.isEmpty(serviceDomain.getPath()) && !StringUtils.isEmpty(serviceDomain.getCmad_start()) && !StringUtils.isEmpty(serviceDomain.getService_name())) {
+            model.addAttribute("InfoSucc", "服务数据存入成功!");
+        }
         return "views/add";
     }
 }
